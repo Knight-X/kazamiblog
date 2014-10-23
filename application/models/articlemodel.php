f0f314a2cdd1733617663681d95e0c6713fb3471
Kazami <kazamizero@gmail.com>
2014-10-23 21:52:45 +0800

    update view pagenitation

--- application/models/articlemodel.php
+++ application/models/articlemodel.php
@@ -30,4 +30,36 @@ class ArticleModel extends CI_Model{
 
       return $query->row();
     }
+
+    function countArticlesByUserID($userID){
+      $this->db->select("count(articleID) as ArticleCount");
+
+      $this->db->from('article');
+      $this->db->where(array("author" => $userID));
+      $query = $this->db->get();
+      
+      if ($query->num_rows() <= 0){
+        return null;
+      }
+
+
+      return $query->row()->ArticleCount;
+    }
+
+
+    function getArticlesByUserID($userID, $offset = 0, &pageSize = 20){
+      $this->db->select("article.*, user.Acount");
+ 
+      $this->db->from('article');
+      $this->db->join('user', 'article.author = user.userID', 'left');
+      $this->db->where(array("author" => $userID));
+
+      $this->db->limit($pageSize, $offset);
+
+      $this->db->order_by("ArticleID", "desc");
+
+      $query = $this->db->get();
+
+      return $query->result();
+    }
   }
--- application/views/article_author.php
+++ application/views/article_author.php
@@ -1,13 +1,58 @@
-<!DOCTYPE html>
-<html>
-  <head>
-    <meta charset="utf-8">
-    <title>Post System-List</title>
-  </head>
-  <body>
+<?php include("_site_header.php"); ?>
+<div class="container article-view">
+  <?php include("_content_nav.php"); ?>
+  <!-- content -->
+  <div class="content">
+    <h1><?=$user->Account ?></h1>
+    
+    <?php foreach($results as $article){ ?>
+      <table class="table table-bordered">
+        <tr>
+          <td width="50">Title</td>
+          <td>
+            <a href="<?=site_url("article/view/".$article->ArticleID) ?>">
+            <?=htmlspecialchars($article->Title) ?></a>
+          </td>
+        </tr>
+        <tr>
+          <td>Content</td>
+          <td><?=n12br(htmlspecialchars($article->Content)) ?></td>
+        </tr>
+      </table>
+      <?php } ?>
 
-    <div id="container">
-      This is the content of the list
+      <p>
+        <?=$pageLinks?>
+      </p>
+
     </div>
-  </body>
-</html>
+  </div>
+  <?php include("_site_footer.php"); ?>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
