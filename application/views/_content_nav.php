a28d61fc9cbb0b2c15374770cc376222612d685c
Kazami <kazamizero@gmail.com>
2014-10-25 14:35:15 +0800

    REVISE THE CONTENT OF THE NAVIGATION BAR

--- application/views/_content_nav.php
+++ application/views/_content_nav.php
@@ -1,16 +1,15 @@
 <!-- Content Header -->
-<div class="navbar navbar-inverse navbar-fixed-up">
-<div class="navbar-inner">
+<div class="nav">
    <div class="container">
        <a class="brand" href="<?=site_url("/")?>">The Articles</a>
-       <ul class="nav">
+       <ul class="pull-left">
          <li class="active"><a href="<?=site_url("/")?>">Home</a></li>
          <?php if (isset($_SESSION["user"]) && $_SESSION["user"] != null) { ?>
          <li><a href="<?=site_url("article/author/".$_SESSION["user"]->Account) ?>">My Articles</a></li>
        </ul>
          <?php } ?>
        <!-- login status -->
-       <ul class="nav">
+       <ul class="pull-right">
        <?php
        if(isset($_SESSION["user"]) && $_SESSION["user"] != null){ ?>
           <li><a href="#">Hi<?=$_SESSION["user"]->Account?></a></li>
@@ -23,4 +22,10 @@
      <?php } ?>
     </div>
   </div>
-</div>
+
+
+  <div class="jumbotron">
+    <div class="container">
+      <h1>Kazami</h1>
+    </div>
+  </div>
