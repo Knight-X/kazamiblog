22b2c5779270c7773d1d1daaec15bb3dd4ac76e4
Kazami <kazamizero@gmail.com>
2014-11-03 23:39:24 +0800

    achieve the view of the upload

--- application/views/upload_form.php
+++ application/views/upload_form.php
@@ -5,15 +5,34 @@
    <div class="content">
 
     <?php echo $error; ?>
+
+      <form action="<?=site_url("upload/set_script"); ?>" method="post">
+        <table>
+          <tr>
+             <td>First movie</td>
+               <td><input type="text" name="first" /></td>
+          </tr>
+          <tr>
+             <td>Second movie</td>
+             <td><input type="text" name="second" /></td>
+          </tr>
+          <tr>
+             <td>Third movie</td>
+             <td><input type="text" name="third" /></td>
+          </tr>
+        </table>
+      </form>
     
-    <?php echo form_open_multipart('upload/do_upload'); ?>
+      <div class="uploadform">
+      <?php echo form_open_multipart('upload/do_upload'); ?>
 
-    <input type="file" name="userfile" size="20" />
-    <br /><br />
+      <input type="file" name="userfile" size="20" />
+      <br /><br />
 
-    <input type="submit" value="upload" />
+      <input type="submit" value="upload" />
 
-    </form>
+      </form>
+    </div>
    </div>
  </div>
   <?php include("_site_footer.php"); ?>
